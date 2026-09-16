dea9e6731c987dc081f6c82454c407a983f73b5d
Raul Belloso Medina <raul.belloso.m@gmail.com>
2026-09-16 06:38:26 -0600

    fix(identity): use correct LoadingState prop name in UserDetailScreen

--- apps/desktop/src/modules/runly.identity/screens/UserDetailScreen.jsx
+++ apps/desktop/src/modules/runly.identity/screens/UserDetailScreen.jsx
@@ -97,7 +97,7 @@ export default function UserDetailScreen() {
   if (userQuery.isLoading) {
     return (
       <div className="p-4 md:p-6">
-        <LoadingState title="Cargando usuario" />
+        <LoadingState message="Cargando usuario" />
       </div>
     );
   }
